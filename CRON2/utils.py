323af20a6f0e4b4d034ed5f53f355ead0c23de08
Livingstone Maxwell <106387735+Stonetech66@users.noreply.github.com>
2023-03-30 13:58:47 +0100

    Update utils.py

--- CRON2/utils.py
+++ CRON2/utils.py
@@ -2,16 +2,36 @@ from datetime import datetime, timedelta
 from dateutil.relativedelta import relativedelta, MO, TU, TH, WE, FR, SA, SU
 import pytz
 
-def find_upper_execution(last_execution:datetime, timezone:str, year:int, month:int, weekday, day:int, hours:int, minutes:int):
+def find_upper_execution(timezone:str, year:int , month:int, weekday, day:int, hours:int, minutes:int):
+
     if not month == 0:
         return datetime.now(tz=pytz.timezone(timezone))+ relativedelta(month=month, months=1, hour=hours, minute=minutes)
-    elif weekday:
-        return last_execution+relativedelta(weekday=eval(weekday)(1))
-    elif not day == 0:
-        return last_execution.replace(tzinfo=pytz.timezone(timezone)) + relativedelta( days=day)
-    else:
-        return last_execution.replace(tzinfo=pytz.timezone(timezone)) + relativedelta( hours=hours, minutes=minutes)
 
+
+    elif weekday:
+        weekdays = {'MO': MO, 'TU': TU, 'WE': WE, 'TH': TH, 'FR': FR, 'SA': SA, 'SU': SU}
+        dt_now = datetime.now(pytz.timezone(timezone))
+        next_dt = dt_now + relativedelta(weekday=weekdays[weekday], hour=hours, minute=minutes, second=0, microsecond=0)
+        if str(dt_now.strptime('%a')[:2]).upper() == weekday and next_dt <= dt_now:
+            date=dt_now + relativedelta(weekday=weekdays[weekday](+1), days=+1, hour=hours, minute=minutes, second=0, microsecond=0)
+        elif str(dt_now.strptime('%a')[:2]).upper() == weekday and next_dt > dt_now:
+            date=dt_now + relativedelta(weekday=weekdays[weekday](+1), hour=hours, minute=minutes, second=0, microsecond=0)
+        else:
+            date=dt_now + relativedelta(weekday=weekdays[weekday](+1), hour=hours, minute=minutes, second=0, microsecond=0) 
+            
+        print(f"weekday found{date}") 
+        return date
+    elif not day == 0:
+        d=datetime.now(tz=pytz.timezone(timezone))
+        if d + relativedelta(hour=hours, minute=minutes, second=0, microsecond=0) < d:
+            return datetime.now(tz=pytz.timezone(timezone))+ relativedelta(days=day, hour=hours, minute=minutes, second=0, microsecond=0)
+        return d + relativedelta(hour=hours, minute=minutes, second=0, microsecond=0)
+    
+    elif not hours == 0:
+        return datetime.now(tz=pytz.timezone(timezone))+ relativedelta(hours=hours, minutes=minutes, second=0, microsecond=0)
+    
+    elif minutes:
+        return datetime.now(tz=pytz.timezone(timezone))+ relativedelta(minutes=minutes, second=0, microsecond=0)
 
 def error_code(status_code):
     if status_code > 400:
