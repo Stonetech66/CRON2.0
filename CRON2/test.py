a1ca8371d8c7c3aecefc1f6f1d18c851a8956531
Livingstone Maxwell <106387735+Stonetech66@users.noreply.github.com>
2023-03-30 17:43:10 +0100

    Update test.py

--- CRON2/test.py
+++ CRON2/test.py
@@ -7,20 +7,22 @@ async def fetch(session, url, c, token):
              return  response.status
 
 
-async def start_test(s_url,token,end): 
+async def start_test(s_url,token,end, url, no, min, hour): 
      c={
-  "url": "https://example.com",
+  "url": f"{url}",
   "method": "get",
   "headers": {
     "Authorization": "Bearer xxxxxxx"
   },
   "timezone": "Africa/Lagos",
-  "minutes": 5,
+  "days" :1,
+  "hours":hour, 
+  "minutes": min,
   "notify_on_error": False
 }                       
      async with aiohttp.ClientSession() as session:
              tasks=[]
-             for u in [f'{end}/v1/add-cron/'] * 500:
+             for u in [f'{end}/v1/add-cron/'] * no:
                      tasks.append(asyncio.ensure_future(fetch(session, u, c, token)))
              responses= await asyncio.gather(*tasks)
              return responses
