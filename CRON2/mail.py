5ebe5245b7a3ed9a6319134b510d1c39de570225
Livingstone Maxwell <106387735+Stonetech66@users.noreply.github.com>
2023-03-31 19:18:52 +0100

    Update mail.py

--- CRON2/mail.py
+++ CRON2/mail.py
@@ -11,6 +11,48 @@ password=os.getenv("MAIL_PASSWORD")
 username=os.getenv("MAIL_USERNAME")
 mail_from=os.getenv("MAIL_FROM")
 
+STATUS_CODES={
+    400: "Bad Request",
+    401: "Unauthorized",
+    402: "Payment Required",
+    403: "Forbidden",
+    404: "Not Found",
+    405: "Method Not Allowed",
+    406: "Not Acceptable",
+    407: "Proxy Authentication Required",
+    408: "Request Timeout",
+    409: "Conflict",
+    410: "Gone",
+    411: "Length Required",
+    412: "Precondition Failed",
+    413: "Payload Too Large",
+    414: "URI Too Long",
+    415: "Unsupported Media Type",
+    416: "Range Not Satisfiable",
+    417: "Expectation Failed",
+    418: "I'm a teapot",
+    421: "Misdirected Request",
+    422: "Unprocessable Entity",
+    423: "Locked",
+    424: "Failed Dependency",
+    425: "Too Early",
+    426: "Upgrade Required",
+    428: "Precondition Required",
+    429: "Too Many Requests",
+    431: "Request Header Fields Too Large",
+    451: "Unavailable For Legal Reasons",
+    500: "Internal Server Error",
+    501: "Not Implemented",
+    502: "Bad Gateway",
+    503: "Service Unavailable",
+    504: "Gateway Timeout",
+    505: "HTTP Version Not Supported",
+    506: "Variant Also Negotiates",
+    507: "Insufficient Storage",
+    508: "Loop Detected",
+    510: "Not Extended",
+    511: "Network Authentication Required"
+}
 
 env_config = ConnectionConfig(
    MAIL_USERNAME=username,
@@ -29,9 +71,9 @@ async def send_error_email(data):
 
 
     message=MessageSchema(
-        subject=f'Cron Job Failed',
+        subject=f'CronJob Failed 😞',
         recipients=[data['email'],],
-        template_body={'status_code':data['code'], 'cron':data['cron'] },
+        template_body={'status_code':data['code'], 'cron':data['cron'], 'message':STATUS_CODES[int(data['code'])] },
         subtype='html'
 
     )
