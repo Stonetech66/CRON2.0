91b858d71cd8a1acc19d8c3c4f1de51f1ad914cc
Livingstone Maxwell <106387735+Stonetech66@users.noreply.github.com>
2023-03-31 07:14:06 +0100

    Update worker.py

--- CRON2/core/worker.py
+++ CRON2/core/worker.py
@@ -5,30 +5,32 @@ import pytz, ssl
 import aiohttp
 from ..utils import next_execution, error_code
 from bson.objectid import ObjectId
-import json
+from json import JsonEncoder
 from aiokafka import AIOKafkaProducer
 import os
 from dotenv import load_dotenv
 import os
-
 load_dotenv()
-server=os.getenv('KAFKA_SERVER')
-kafka_password=os.getenv("KAFKA_PASSWORD")
-kafka_username= os.getenv("KAFKA_USERNAME")
-CRON_MAXIMUM_FAILURES=1 
-class CustomJsonEncoder(json.JSONEncoder):
+KAFKA_SERVER = os.getenv('KAFKA_SERVER')
+KAFKA_PASSWORD = os.getenv("KAFKA_PASSWORD")
+KAFKA_USERNAME = os.getenv("KAFKA_USERNAME")
+CRON_TOPIC = 'cron-2'
+CRON_MAX_FAILURES=3 
+REQUEST_TIMEOUT=30 #time in seconds
+logger = logging.getLogger(__name__)
+
+class CustomJsonEncoder(JSONEncoder):
     def default(self, o):
         if isinstance(o, datetime):
             return o.isoformat()
         return super().default(o)
 
-
-timeout=aiohttp.ClientTimeout(total=30)
-
-
+timeout=aiohttp.ClientTimeout(total=REQUEST_TIMEOUT)
+max_failures=CRON_MAX_FAILURES
 
 
-async def send_requests(session, data, producer):
+
+async def send_request(session, data, producer):
     method=data['method']
     header=data['header']
     schedule= data['schedule']
@@ -37,41 +39,22 @@ async def send_requests(session, data, producer):
     cron_id= ObjectId(data['cron_id'])
     body= data['body']
     email=data["email"]
-    print(f"{url} {datetime.now()}")
+    logger.info(f"{url} {datetime.now()}")
     # sending http requests to the specified endpoint
     try:
-            if method == "get":
-                    async with session.get(url, timeout=timeout, headers=header) as response:
-                        status_code= response.status
-            elif method== "post":
-                async with session.post(url, timeout=timeout, headers=header, json=body) as response:
-                    status_code= response.status
-            elif method == "put":
-                async with session.put(url, timeout=timeout, headers=header, json=body) as response:
-                    status_code= response.status
-            elif method== "delete":
-                async with session.delete(url, timeout=timeout, headers=header) as response:
-                    status_code= response.status
-            elif method == "head":
-                async with session.head(url, timeout=timeout, headers=header) as response:
-                    status_code= response.status
-            elif method == "options":
-                async with session.options(url, timeout=timeout, headers=header) as response:
-                    status_code= response.status
-            elif method == "patch":
-                async with session.patch(url, timeout=timeout, headers=header) as response:
-                    status_code= response.status
-            else: 
-                status_code = 500
-    except:
-        status_code=500
+        async with getattr(session, method)(url, timeout=timeout, headers=header, json=body) as response:
+            status_code = response.status
+    except aiohttp.ClientResponseError as e:
+        status_code = e.status
+    except Exception as e:
+        status_code = 500
+        logger.error(f"request error {e} ") 
+
+    await save_response(schedule, cron_id, status_code,email, producer)
+    return {"status":status_code, "url":url, "cron_id":cron_id, "timestamp":datetime.utcnow()}
 
 
-    await save_responses(schedule, cron_id, status_code,email, producer)
-    return {"status_code":status_code, "cron_id":cron_id, "date":datetime.now()}
-
-
-async def save_responses(schedule:dict, cron_id, status_code, email, producer):
+async def save_response(schedule:dict, cron_id, status_code, email, producer):
     year= schedule["years"]
     month=schedule["month"]
     weekday=schedule["weekday"]
@@ -83,20 +66,25 @@ async def save_responses(schedule:dict, cron_id, status_code, email, producer):
     upper_execution= next_execution(timezone, year, month, weekday, day, hours, minute)
 
     if error_code(status_code):
-        await cron_table.update_one({"_id": ObjectId(cron_id)}, {"$set": {"schedule.next_execution": upper_execution}, "$inc": {"error_count": 1}})
-        if schedule['notify_on_error']:
-            await producer.send("error-mail",{"code":status_code, "email":email})
+        try:
+            await cron_table.update_one({"_id": ObjectId(cron_id)}, {"$set": {"schedule.next_execution": upper_execution}, "$inc": {"error_count": 1}})
+            if schedule['notify_on_error']:
+                await producer.send("error-mail",{"code":status_code, "email":email})
+        except Exception as e:
+            logger.exception(f"An exception occurred while updating cron table for cron {cron_id}: {str(e)}")
     else:
-        await cron_table.update_one({"_id":ObjectId(cron_id)}, {"$set": {"schedule.next_execution":upper_execution}})
+        try:
+            await cron_table.update_one({"_id":ObjectId(cron_id)}, {"$set": {"schedule.next_execution":upper_execution}})
+        except Exception as e:
+            logger.exception(f"An exception occurred while updating cron table for cron {cron_id}: {str(e)}")
 
 
 
-
-async def CronJob(producer):
-
+async def cron_job(producer):
+      try:
         tasks=[]
         # filtering through the database to find crons whose schedule time are less than or equal to the current UTC time
-        async for cron in cron_table.find({"schedule.next_execution":{"$lte": datetime.now(tz=pytz.timezone("UTC"))}, "error_count":{"$lt":3}}):
+        async for cron in cron_table.find({"schedule.next_execution":{"$lte": datetime.now(tz=pytz.timezone("UTC"))}, "error_count":{"$lt":max_failures}}):
             next_execution=cron['schedule']["next_execution"]
             timezone=cron["schedule"]["timezone"]
 
@@ -109,40 +97,51 @@ async def CronJob(producer):
                 header=cron["headers"]
                 body=cron["body"]
                 email=cron["user"]["email"]
-                task = asyncio.create_task(producer.send('cron-2', {"url":url, "cron_id":cron_id, "method":method, "schedule":schedule, "header":header, "body":body, "email":email}))
+                task = asyncio.create_task(producer.send(CRON_TOPIC, {"url":url, "cron_id":cron_id, "method":method, "schedule":schedule, "header":header, "body":body, "email":email}))
                 tasks.append(task)
-        start=datetime.now()
         await asyncio.gather(*tasks)
-        print(f"producing finished: {datetime.now() - start}")
+      except Exception as e:
+        logger.error(f"Error in CronJob: {e}")
 
 
 
-async def Startcron():
-    producer=AIOKafkaProducer(
-    bootstrap_servers=[server], 
-    security_protocol="SASL_SSL", sasl_mechanism="PLAIN", 
-    sasl_plain_username=kafka_username, 
-    sasl_plain_password=kafka_password, 
-    ssl_context=ssl.create_default_context(),compression_type="gzip", value_serializer=lambda x: json.dumps(x, cls=CustomJsonEncoder).encode('utf-8'))
-    try:
+
+async def start_cron():
+    producer_conf = {
+        'bootstrap_servers': [KAFKA_SERVER],
+        'security_protocol': "SASL_SSL",
+        'sasl_mechanism': "PLAIN",
+        'sasl_plain_username': KAFKA_USERNAME,
+        'sasl_plain_password': KAFKA_PASSWORD,
+        'ssl_context': ssl.create_default_context(),
+        'compression_type': "gzip",
+        'batch_size':32768, 
+        'value_serializer': lambda x: json.dumps(x, cls=CustomJsonEncoder).encode('utf-8'),
+    }
+
+    async with AIOKafkaProducer(**producer_conf) as producer:
+      try:
         await producer.start()
-        print("worker started 🚀")
+        logger.info("worker started 🚀")
         while True:
 
             '''A loop that occurs every 1 minute , and calls the main coroutine 
             which then finds any cron which its scheduled time is at this particular minute
             '''
-            st=datetime.now()
-            await CronJob(producer)
-            print(datetime.now() - st)
-            next_minute = (datetime.now() + timedelta(minutes=1)).replace(second=0, microsecond=0)
-            delay = (next_minute - datetime.now()).total_seconds()
+            st=datetime.utcnow()
+            await cron_job(producer)
+            logger.info(f"Producing finished {datetime.now() - st} ")
+            next_minute = (datetime.utcnow() + timedelta(minutes=1)).replace(second=0, microsecond=0)
+            delay = (next_minute - datetime.utcnow()).total_seconds()
             await asyncio.sleep(delay)
-    finally:
-        await producer.stop()
+      except Exception as e:
+        logger.error(f"Error in Producer: {e}")
+      finally:
+         await producer.stop()
 
 if __name__ == "__main__":
-    asyncio.run(Startcron())
+    logging.basicConfig(level=logging.INFO, format="%(asctime)s:%(levelname)s:%(message)s")
+    asyncio.run(start_cron())
 
 
 
