a803fcf3bd2a99c987deee8c234459613f6053a7
Livingstone Maxwell <106387735+Stonetech66@users.noreply.github.com>
2023-03-31 21:40:59 +0100

    Update test_cron.py

--- CRON2/cron/test_cron.py
+++ CRON2/cron/test_cron.py
@@ -1,2 +1,30 @@
+from fastapi.testclient import TestClient
+
+from ..main import app
+
+client = TestClient(app)
 
 
+def test_get_crons():
+    pass
+
+def test_get_cron():
+     pass
+
+def test_update_cron():
+    pass
+
+def test_delete_cron():
+   pass
+
+def test_get_cron_response_history():
+    pass
+
+def test_delete_cron_response_history():
+    pass
+
+def test_get_response():
+   pass
+
+def test_delete_response():
+   pass
