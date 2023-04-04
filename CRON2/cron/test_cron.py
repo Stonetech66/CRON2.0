fad92a24778d98e26b793300c92476743f24adad
Livingstone Maxwell <106387735+Stonetech66@users.noreply.github.com>
2023-04-04 18:02:53 +0100

    Update test_cron.py

--- CRON2/cron/test_cron.py
+++ CRON2/cron/test_cron.py
@@ -2,7 +2,7 @@ from fastapi.testclient import TestClient
 from fastapi import HTTPException 
 from ..main import app
 from unittest.mock import MagicMock
-from ..dependencies import get_currrent_user 
+from ..dependencies import get_current_user 
 from ..crud import Cron
 client = TestClient(app)
 
@@ -15,21 +15,21 @@ unauth_user= HTTPException(detail='invalid token or token has expired', status_c
 auth_header={'Authorization': 'Bearer Xxxxxxxxxxx'}
 def test_get_crons():
     # Authenticated Request 
-    get_currrent_user= MagicMock(return_value=auth_user) 
+    get_current_user= MagicMock(return_value=auth_user) 
     Cron.get_crons=MagicMock(return_value=[]) 
     resp=client.get('api/v1/crons', headers=auth_header)
     assert resp.status_code == 200
     assert resp.json() == []
 
     # Unauthenticated Request 
-    get_currrent_user= MagicMock(side_effect=unauth_user)
+    get_current_user= MagicMock(side_effect=unauth_user)
     resp=client.get('/api/v1/crons')
     assert resp.status_code == 401
 
 
 def test_get_cron():
     # Authenticated Request 
-    get_currrent_user=MagicMock(return_value=auth_user)
+    get_current_user=MagicMock(return_value=auth_user)
     response_data={
    "id": "642c2d7ea0209c97a399b860","url": "https://example.com","method": "get",
     "headers": {"Authorization": "Bearer xxxxxxx" },
@@ -43,13 +43,13 @@ def test_get_cron():
     assert resp.json() == cron_data
 
     # Unauthenticated Request 
-    get_currrent_user= MagicMock(side_effect=unauth_user)
+    get_current_user= MagicMock(side_effect=unauth_user)
     resp=client.get('/api/v1/cron/642c2d7ea0209c97a399b860')
     assert resp.status_code == 401
     assert resp.json() == auth_error
     
     # Invalid Cron Id
-    get_currrent_user= MagicMock(return_value=auth_user)
+    get_current_user= MagicMock(return_value=auth_user)
     Cron.get_cron= MagicMock(side_effect=HTTPException(status_code=404, detail='invalid Cron ID'))
     resp=client.get('/api/v1/cron/642c2d7ea0209c97a399b899')
     assert resp.status_code == 404
@@ -58,7 +58,7 @@ def test_get_cron():
 
 def test_update_cron():
     # Authenticated Request 
-    get_currrent_user= MagicMock(return_value=auth_user)
+    get_current_user= MagicMock(return_value=auth_user)
     response_data={"id": "642c2d7ea0209c97a399b860", "url":"https://example2.com","method": "get",
     "headers": { "Authorization": "Bearer xxxxxxx" }, "body": None,"schedule": {
     "notify_on_error": False,"years": 0,"month": 0,
@@ -72,12 +72,12 @@ def test_update_cron():
     assert resp.json() == cron_data 
 
     # Unauthenticated Request 
-    get_currrent_user= MagicMock(side_effect=unauth_user)
+    get_current_user= MagicMock(side_effect=unauth_user)
     resp=client.put('/api/v1/update-cron/642c2d7ea0209c97a399b860', json=update_json)
     assert resp.status_code == 401
    
     # invalid Cron ID 
-    get_currrent_user= MagicMock(return_value=auth_user)
+    get_current_user= MagicMock(return_value=auth_user)
     Cron.update_cron = MagicMock(side_effect=HTTPException(status_code=404, detail='invalid Cron ID'))
     resp=client.put('/api/v1/update-cron/642c2d7ea0209c97a399b756', headers=auth_header, json=json)
     assert resp.status_code == 404
@@ -85,18 +85,18 @@ def test_update_cron():
 
 def test_delete_cron():
     #Authenticated Request
-    get_currrent_user= MagicMock(return_value=auth_user)
+    get_current_user= MagicMock(return_value=auth_user)
     Cron.delete_cron=MagicMock(return_value=True) 
     resp=client.delete('/api/v1/delete-cron/642c2d7ea0209c97a399b860', headers=auth_header)
     assert resp.status_code == 204
 
     # Unauthenticated Request 
-    get_currrent_user= MagicMock(return_value=unauth_user)
+    get_current_user= MagicMock(return_value=unauth_user)
     resp=client.delete('/api/v1/delete-cron/642c2d7ea0209c97a399b860')
     assert resp.status_code == 401
 
     # invalid Cron ID 
-    get_currrent_user= MagicMock(return_value=auth_user)
+    get_current_user= MagicMock(return_value=auth_user)
     Cron.delete_cron = MagicMock(side_effect=HTTPException(status_code=404, detail='invalid Cron ID'))
     resp=client.delete('/api/v1/delete-cron/642c2d7ea0209c97a399b560', headers=auth_header)
     assert resp.status_code == 404
